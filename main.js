5b74c418876f073e395c497a033f562390a6224f
Sean Gu <zonggu@Seans-MacBook-Pro.local>
2018-09-01 13:25:19 +0800

    https://github.com/audiolabhk/spa_template.git

---
+++ main.js
@@ -0,0 +1,11 @@
+home()
+
+function home() {
+    $('#write').load('/pages/home.html')
+    console.log('home')
+}
+
+function about() {
+    $('#write').load('/pages/about.html')
+    console.log('home')
+}
